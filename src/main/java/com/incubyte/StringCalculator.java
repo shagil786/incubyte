7d90d0dc75f3de828a49b9609bbbe25bf19acc93
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2024-06-08 21:13:19 +0530

    Support different delimiters works after adding code in production

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -5,9 +5,16 @@ public class StringCalculator {
         if(numbers.isEmpty()) {
             return 0;
         }
+
+        String delimiter = ",";
+        if(numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
         
-        numbers = numbers.replace("\n", ",");
-        String[] parts = numbers.split(",");
+        numbers = numbers.replace("\n", delimiter);
+        String[] parts = numbers.split(delimiter);
         int sum = 0;
         for(String part : parts) {
             sum += Integer.parseInt(part);
