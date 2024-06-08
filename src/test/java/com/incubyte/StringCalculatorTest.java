9f247fc8d1462e540dfa9d0a45f09bcf90be4d9a
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2024-06-08 20:46:26 +0530

    Test Case fails - not implemented in production code.

--- src/test/java/com/incubyte/StringCalculatorTest.java
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -9,4 +9,10 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void testAddSingleNumber() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add('1'));
+    }
 }
